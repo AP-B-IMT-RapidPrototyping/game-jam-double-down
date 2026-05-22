70ad66b527b917bb01de09748fd3223397cc6679
CasuallySleepy <145465388+CasuallySleepy@users.noreply.github.com>
2026-05-22 10:07:21 +0200

    Updated main.cpp

--- GoinDown/main.cpp
+++ GoinDown/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include "raylib.h"
+
+
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+#define FPS 60
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Goin' Down");
+    SetTargetFPS(FPS);
 
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(DARKBLUE);
+
+        // Hey :)
+
+        EndDrawing();
+    }
 
     return 0;
 }
