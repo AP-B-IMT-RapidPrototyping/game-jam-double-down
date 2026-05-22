f8ee76d5a9dbea4b876ff9f5883cb78e9c14ca9b
Bumblebee10 <123257052+Bumblebee10@users.noreply.github.com>
2026-05-22 15:58:53 +0200

    Tests and held deleted

--- GoinDown/tests/test_main.cpp
+++ GoinDown/tests/test_main.cpp
@@ -1,22 +1,46 @@
 #include <catch2/catch_test_macros.hpp>
-#include "Held.h"
+
+#include "FloatingEntity.h"
+#include "Player.h"
+#include "PressureBar.h"
 
 TEST_CASE("sanity check", "[Setup]") {
-    Game::Held m = Game::Held();
+    Player p = Player();
 
-    REQUIRE_NOTHROW(m.GetPosition());
+    REQUIRE_NOTHROW(p.GetHealth());
 }
 
-TEST_CASE("Held", "[Held][spawnposition][x]") {
-    Game::Held _hero = Game::Held();
-    SECTION("positie") {
-        SECTION("X") {
-            SECTION("Groterdan 0") {
-                REQUIRE(_hero.GetPosition().x>0);
-            }
-            SECTION("kleinerdan 800") {
-                REQUIRE(_hero.GetPosition().x<800);
-            }
-        }
+TEST_CASE("Pressurebar", "[PressureBar]") {
+    PressureBar pressureBar = PressureBar();
+    SECTION("pressure can change") {
+        int currentPressure = pressureBar.GetPressure();
+        pressureBar.ChangePressure(20);
+        int newPressure = pressureBar.GetPressure();
+        REQUIRE(newPressure == currentPressure + 20);
+    }
+    SECTION("pressure can reset") {
+        pressureBar.ChangePressure(50);
+        pressureBar.ResetPressure();
+        REQUIRE(pressureBar.GetPressure() == 0);
+    }
+}
+
+TEST_CASE("FloatingEntity", "[FloatingEntity]") {
+    float velocity = -5;
+    FloatingEntity floatingEntity = FloatingEntity(true, velocity);
+    SECTION("Can get position") {
+        REQUIRE_NOTHROW(floatingEntity.GetPos());
+    }
+    SECTION("Can get size") {
+        REQUIRE_NOTHROW(floatingEntity.GetSize());
+    }
+    SECTION("Can get collision") {
+        REQUIRE_NOTHROW(floatingEntity.GetCollision());
+    }
+    SECTION("Position gets updated correctly") {
+        Vector2 currentPos = floatingEntity.GetPos();
+        floatingEntity.Update();
+        currentPos.y += velocity;
+        REQUIRE(floatingEntity.GetPos().y == currentPos.y);
     }
 }
